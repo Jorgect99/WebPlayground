3fa29c234afcff719b0615c5d27a5c1543e28c5e
Jorge <jorgect99@gmail.com>
2020-12-20 12:41:04 -0700

    Vistas CRUD con CBV: UpdateView

--- WebPlayground/urls.py
+++ WebPlayground/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.urls import pages_patterns
 
 urlpatterns = [
     #core
     path('', include('core.urls')),
     #pages
-    path('pages/', include('pages.urls')),
+    path('pages/', include(pages_patterns)),
     #admin
     path('admin/', admin.site.urls),
 ]
