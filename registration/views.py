226afa458b8cec893745f0b8edd6248c2d968aad
Jorge <jorgect99@gmail.com>
2020-12-20 19:47:15 -0700

    Validando un email unico

--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
+
+
 # Create your views here.
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
@@ -17,10 +19,10 @@ class SignUpView(CreateView):
         #Modificar en tiempo real
         form.fields['username'].widget = forms.TextInput(attrs={
             'class':'form-control mb-2', 'placeholder':'Nombre de Usuario'})
-
+        form.fields['email'].widget = forms.EmailInput(attrs={
+            'class':'form-control mb-2', 'placeholder':'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={
             'class':'form-control mb-2', 'placeholder':'Clave'})
-            
         form.fields['password2'].widget= forms.PasswordInput(attrs={
             'class':'form-control mb-2', 'placeholder':'Repetir clave'})
         return form 
